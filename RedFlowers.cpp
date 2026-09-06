c8225c0ee5dfab0ec3dbfacee3c1679d93c466da
LUUXO <granbell56@gmail.com>
2026-09-06 20:20:31 +0200

    Enhance console output with color formatting
    Added color support for console output in main function.

--- RedFlowers.cpp
+++ RedFlowers.cpp
@@ -5,13 +5,14 @@
 #include <chrono>
 
 #include "PONT.hpp"
+#include "color.hpp"
 
 int main(int argc, char* argv[]) {
 
     configure_console();
     std::cout << "\033[2J\033[1;1H";
 
-    std::cout << "RedFlower's - C++ Edition" << std::endl;
+    std::cout << Color::DARK_GRAY <<  "RedFlower's - C++ Edition" << Color::RESET << std::endl;
 
     std::cout  << "\n" << std::endl;
 
